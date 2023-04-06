3dd1e1c051d13b204e4cd3f79ebdd2b33233523c
thomsonbel12 <thomsonbel12@gmail.com>
2023-04-06 21:11:17 +0700

    m

--- src/main/java/com/example/tlstore/TlStoreApplication.java
+++ src/main/java/com/example/tlstore/TlStoreApplication.java
@@ -11,8 +11,8 @@ public class TlStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(TlStoreApplication.class, args);
         System.out.println("-----------------------------------------------------------");
-        System.out.println("🚀 Server ready at http://localhost:8080");
-        System.out.println("🚀 Api doc ready at http://localhost:8080/swagger-ui.html ");
+        System.out.println("🚀 Server ready at http://localhost:8081");
+        System.out.println("🚀 Api doc ready at http://localhost:8081/swagger-ui.html ");
     }
 
 }
